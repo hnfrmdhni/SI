d0e6c55269076f9c11c151316ebf6cd6eb4fba0d
hnfrmdhni <91604141+hnfrmdhni@users.noreply.github.com>
2022-09-23 09:09:21 +0700

    23/09/22

--- application/views/v_mywelcome.php
+++ application/views/v_mywelcome.php
@@ -2,79 +2,81 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sistem Informasi</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Sistem Informasi</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+		integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
 </head>
 
 <body>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
-        integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
-    </script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js"
-        integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous">
-    </script>
-    <h2 align="center">Hello World!</h2>
+	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
+		integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
+	</script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js"
+		integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous">
+	</script>
+	<h2 align="center">Hello World!</h2>
+	<h3 align="center">This is v_mywelcome</h3>
 
-    <div class="container text-center">
-        <div class="row">
-            <div class="col">
-                Column 1
-                <table class="table table-hover table-bordered table-striped">
-                    <thead class="table-dark">
-                        <tr>
-                            <th scope=" col">#</th>
-                            <th scope="col">First Name</th>
-                            <th scope="col">Last Name</th>
-                            <th scope="col">Username</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <th scope="row">1</th>
-                            <td>Hanif</td>
-                            <td>Ramadhani</td>
-                            <td>@hanif</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">2</th>
-                            <td>Dhea</td>
-                            <td>Ramadhani</td>
-                            <td>@dhea</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">3</th>
-                            <td>Dellin</td>
-                            <td>Irawan</td>
-                            <td>@dellin</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">4</th>
-                            <td>Vanya</td>
-                            <td>Ardani</td>
-                            <td>@vanya</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-            <div class="col">
-                Column 2
-                <p align="justify">
-                    Lorem ipsum dolor sit amet. Sed deleniti quia rem eligendi consectetur sed fuga nihil et rerum
-                    ducimus rem modi minima. Eum ratione consequatur ut voluptatibus repudiandae sed doloribus assumenda
-                    qui quidem obcaecati sit maiores voluptatibus? Quia minima qui autem tempore qui maxime voluptatem
-                    qui minus officia qui voluptatem iure.
-                    Ad consequatur quasi est tenetur eaque vel nulla explicabo sed dolorem nisi! Aut debitis architecto
-                    ea quasi laborum non nihil fugit est quos cupiditate. Cum similique enim ut quidem obcaecati est
-                    nemo animi ut optio sequi cum nesciunt quaerat et suscipit totam est minima commodi.
-                </p>
-            </div>
-        </div>
-    </div>
-    </div>
+	<div class="container text-center">
+		<div class="row">
+			<div class="col">
+				Column 1
+				<table class="table table-hover table-bordered table-striped caption-top">
+					<caption>Ini adalah list user : </caption>
+					<thead class="table-dark">
+						<tr>
+							<th scope=" col">#</th>
+							<th scope="col">First Name</th>
+							<th scope="col">Last Name</th>
+							<th scope="col">Username</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<th scope="row">1</th>
+							<td>Hanif</td>
+							<td>Ramadhani</td>
+							<td>@hanif</td>
+						</tr>
+						<tr>
+							<th scope="row">2</th>
+							<td>Dhea</td>
+							<td>Ramadhani</td>
+							<td>@dhea</td>
+						</tr>
+						<tr>
+							<th scope="row">3</th>
+							<td>Dellin</td>
+							<td>Irawan</td>
+							<td>@dellin</td>
+						</tr>
+						<tr>
+							<th scope="row">4</th>
+							<td>Vanya</td>
+							<td>Ardani</td>
+							<td>@vanya</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+			<div class="col">
+				Column 2
+				<p align="justify">
+					Lorem ipsum dolor sit amet. Sed deleniti quia rem eligendi consectetur sed fuga nihil et rerum
+					ducimus rem modi minima. Eum ratione consequatur ut voluptatibus repudiandae sed doloribus assumenda
+					qui quidem obcaecati sit maiores voluptatibus? Quia minima qui autem tempore qui maxime voluptatem
+					qui minus officia qui voluptatem iure.
+					Ad consequatur quasi est tenetur eaque vel nulla explicabo sed dolorem nisi! Aut debitis architecto
+					ea quasi laborum non nihil fugit est quos cupiditate. Cum similique enim ut quidem obcaecati est
+					nemo animi ut optio sequi cum nesciunt quaerat et suscipit totam est minima commodi.
+				</p>
+			</div>
+		</div>
+	</div>
+	</div>
 </body>
 
 </html>
